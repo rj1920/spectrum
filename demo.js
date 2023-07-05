efcddd4a7d2847f01c5a25541869a1f52743a86d
riyaj <rmulla12@gmail.com>
2023-07-05 20:29:26 +0530

    First Commit

--- demo.js
+++ demo.js
@@ -1,6 +1,3 @@
 console.log("HAHAHAHlala");
 console.log("afghsd");
 console.log("B1 Added code from here")
-console.log("HAHAHAHlala");
-console.log("afghsd");
-console.log("B1 Added code from here")
--- demo1.js
+++ demo1.js
@@ -1,6 +1,1 @@
 console.log("HAHAHAHlala");
-console.log("afghsd");
-console.log("sgtdrj");
-console.log("HAHAHAHlala");
-console.log("afghsd");
-console.log("sgtdrj");
