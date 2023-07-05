570f1950eed9cde6fe16cebf560aaaa48ca881f3
riyaj <rmulla12@gmail.com>
2023-07-05 20:34:40 +0530

    B1 Commited

--- demo.js
+++ demo.js
@@ -1,5 +1,4 @@
-console.log("HAHAHAHlala");
-console.log("afghsd");
+
 console.log("B1 Added code from here")
 console.log("HAHAHAHlala");
 console.log("afghsd");
--- demo1.js
+++ demo1.js
@@ -2,5 +2,3 @@ console.log("HAHAHAHlala");
 console.log("afghsd");
 console.log("sgtdrj");
 console.log("HAHAHAHlala");
-console.log("afghsd");
-console.log("sgtdrj");
